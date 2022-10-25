b04913eec5907952185d4a373d9d60a7e866522b
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-10-25 16:26:53 +0000

    Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # def is_logged_in?
+  #   !!session[:user_id]
+  # end
+
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if is_logged_in?
+  # end
 end
